832b1368566e1f30082c5bd70e52724289242499
kolynchick <nivontev95@gmail.com>
2021-03-10 16:54:50 +0300

    feat: deploy api to aws ec2

--- apps/api/src/app/database/database.module.ts
+++ apps/api/src/app/database/database.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import * as mongoose from 'mongoose';
-import { environment } from '../../environments/environment';
+
 import { DATABASE_CONNECTION_PROVIDE_KEY } from './database.provide';
 
 const databaseConnectionProvider = {
   provide: DATABASE_CONNECTION_PROVIDE_KEY,
   useFactory: (): Promise<typeof mongoose> =>
-    mongoose.connect(environment.mongoDbUri, {
+    mongoose.connect(process.env.MONGO_DB_CLOUD_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     }),
